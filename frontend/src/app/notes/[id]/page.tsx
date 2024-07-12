06f34751bc6d8a90e4fbebbf3f5646deb2e7e066
Belkacem AIT SLIMANI <bkaka75@gmail.com>
2024-07-12 12:29:11 +0200

    chore: Remove dummy note

--- frontend/src/app/notes/[id]/page.tsx
+++ frontend/src/app/notes/[id]/page.tsx
@@ -2,26 +2,11 @@ import AppLayout from "@/components/PageLayout";
 import NoteDetail from "@/components/NoteDetails/NoteDetails";
 import AppContextProvider from "@/contexts/AppContext";
 
-const dummyNote = {
-  title: "Sample Note",
-  content: "This is a sample note content.",
-  history: [
-    {
-      date: "2024-07-01",
-      content: "This is the first version of the note.",
-    },
-    {
-      date: "2024-07-05",
-      content: "This is an updated version of the note.",
-    },
-  ],
-};
-
-export default function Details({
-  params: { id },
-}: {
+interface PageProps {
   params: { id: string };
-}) {
+}
+
+export default function Details({ params: { id } }: PageProps) {
   return (
     <AppContextProvider>
       <AppLayout>
